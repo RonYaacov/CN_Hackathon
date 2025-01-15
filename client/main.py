426ef8cf4824c920c7840e854ae20af107fb34a5
Ron Yaacov <ronyaacov19@gamil.com>
2025-01-15 23:10:18 +0200

    remove prints

--- client/main.py
+++ client/main.py
@@ -8,8 +8,6 @@ def main():
     while True:
         start_up_handler =  StartupHandler()
         start_up_handler.get_amount_of_data()
-        print("The amount of data is: ", start_up_handler.amount_of_data)
-        print("The amount of data in bytes is: ", start_up_handler.amount_of_data_in_bytes)
         print("Client started, listening for offer requests...")
         offer_seeker = OfferSeeker()
         offer_seeker.seek_for_offer()
