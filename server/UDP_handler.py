ce2b17f23bc8aec5e8ab298a557e6a946e3b1528
Ron Yaacov <ronyaacov19@gamil.com>
2025-01-19 10:40:33 +0200

    reuse addr

--- server/UDP_handler.py
+++ server/UDP_handler.py
@@ -8,6 +8,7 @@ class UDPHandler:
     
     def __init__(self):
         self.connection_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.connection_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.workers = []
         
     def listen(self):
